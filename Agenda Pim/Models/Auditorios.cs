cf54a4d11b4745ffd448222a0ec6d2ab0418905f
Eddieamorim <48055625+Eddieamorim@users.noreply.github.com>
2020-05-15 21:07:26 -0300

    xssxs

---
+++ Agenda Pim/Models/Auditorios.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agenda_Pim.Models
+{
+    public class Auditorios
+    {
+        [Key]
+        public int IdAuditorio { get; set; }
+        public string NomeAuditorio { get; set; }
+        public int Capacidade { get; set; }
+        public string ObeservacaoAuditorio { get; set; }
+        public DataTable AgendaAuditorio { get; set; }
+
+    }
+}
